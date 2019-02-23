f9212d02cfa38bcc3492bc5cc65c1b870c02f100
emily-li <emily.li@live.co.uk>
2019-02-23 22:35:12 +0000

    Adding output weights

--- src/main/scala/com/liemily/wordassociation/network/WordNetwork.scala
+++ src/main/scala/com/liemily/wordassociation/network/WordNetwork.scala
@@ -2,13 +2,17 @@ package com.liemily.wordassociation.network
 
 import scala.util.Random
 
-case class WordNetwork(inputWeights: Array[Array[Double]])
+class WordNetwork(val inputWeights: Array[Array[Double]], val outputWeights: Array[Array[Double]])
+
 object WordNetwork {
-  def apply(vocabSize: Int, numLayers: Int): WordNetwork = {
-    val inputWeights = Array.fill(vocabSize, numLayers)(randomiseInputWeight(numLayers))
-    new WordNetwork(inputWeights)
+  def apply(vocabSize: Int, projectionLayerSize: Int): WordNetwork = {
+    val inputWeights = Array.fill(vocabSize, projectionLayerSize)(randomiseInputWeight(projectionLayerSize))
+    val outputWeights = Array.ofDim[Double](vocabSize, projectionLayerSize)
+    new WordNetwork(inputWeights, outputWeights)
   }
 
   def randomiseInputWeight(numLayers: Int): Double = standardise(Random.nextDouble, numLayers)
+  // 0.5 is the median of the weight range permitted
+  // i.e. half way between 0 and 1 provided by Random.nextDouble
   def standardise(weight: Double, numLayers: Int): Double = (weight - 0.5) / numLayers
 }
